4f8910051e5b82b36cdc5db9ebb83d88219b565a
Jonathan Parker <jprkr21@gmail.com>
2022-02-13 13:35:49 -0600

    Setting to localStorage

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
     var today = moment();
     $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+    let timeID = ['#0900', '#1000', '#1100', '#1200', '#1300', '#1400', '#1500', '#1600', '#1700']
+    let momentTime = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+    
     function colorCodedTime() {
 
         var currentHour = moment().hour()
         console.log(currentHour)
-
-        let timeID = ['#0900', '#1000', '#1100', '#1200', '#1300', '#1400', '#1500', '#1600', '#1700']
-        let momentTime = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
             for (let i = 0; i < timeID.length; i++) {
     
@@ -25,6 +25,31 @@ $(document).ready(function () {
 
     }
 
+    $('.saveBtn').on('click', function () {
+
+        let buttonValue = $(this).attr('data-value');
+        console.log(buttonValue)
+
+        let text = $('#' + buttonValue).val(); // gets ID for textarea
+        console.log(text)
+
+        localStorage.setItem('#' + buttonValue, text)
+
+    })
+
+    function saveText() {
+
+        for (let i = 0; i < timeID.length; i++) {
+
+            if (localStorage.getItem(timeID[i])) {
+                $(timeID[i]).val(localStorage.getItem(timeID[i]))
+            }
+            
+        }
+
+    }
+
     colorCodedTime();
+    saveText();
 
 });
